f0724373e7937215c5125b4827eed990e6af50f7
Xicheng_Guo <Xicheng_Guo@wistron.com>
2023-04-03 16:28:04 +0800

    update

--- src/plugin/__init__.py
+++ src/plugin/__init__.py
@@ -1,6 +1,18 @@
 from aqt import mw
-from aqt.qt import QAction
+from aqt.qt import QAction, QMessageBox
 
-action = QAction("AnyToAnki Sync", mw)
-# action.triggered.connect(on_sync_clicked)
-mw.form.menuTools.addAction(action)
+
+def on_sync_clicked():
+    try:
+        1 / 0
+    except Exception as e:
+        QMessageBox.information(mw, "同步成功", str(e))
+
+
+def main():
+    action = QAction("AnyToAnki Sync", mw)
+    action.triggered.connect(on_sync_clicked)
+    mw.form.menuTools.addAction(action)
+
+
+main()
